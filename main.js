575ccbd2fdc3728c3574804ff43a67354605fee6
supertiger1234 <saawalkhn@googlemail.com>
2018-05-05 11:54:59 +0100

    Added weather suggestions list.

--- main.js
+++ main.js
@@ -2,10 +2,24 @@ const electron = require('electron');
 const {app, BrowserWindow} = require('electron');
 const path = require('path');
 const url = require('url');
+const weather = require('openweather-apis');
+const cities = require("cities-list");
+const {ipcMain} = require('electron');
+
+
 
 app.on('ready', ()=> {
-    let win = new BrowserWindow({width:800, height:600, frame: false})
+    let win = new BrowserWindow({width:800, height:600, frame: false});
     win.loadURL(`file://${__dirname}/site/index.html`)
-
-
 });
+
+ipcMain.on('getSuggestions', (event, cityPartial) => {
+    const newCities = [];
+    for(const item of cities) {
+        if (item.name.toLowerCase().includes(cityPartial.toLowerCase())){
+            newCities.push(item)
+        }
+    }
+    event.sender.send('sendSuggestions', newCities)
+});
+
--- site/js/main.js
+++ site/js/main.js
@@ -1,15 +1,48 @@
 $(function() {
-    $("html body").animate({
-        backgroundColor: "#70d1ff"
-    }, 500, function () {
-        $('.weatherChooser').fadeIn('fast')
-            .css("display", "absolute")
-            .hide()
-            .fadeIn()
-            .animate({
-            'top': '0'
-        }, {duration: 'slow', queue: false}, function() {
-            // Animation complete.
-        });
-    } );
+    setTimeout(function(){
+        $("html body").animate({
+            backgroundColor: "#70d1ff"
+        }, 500, function () {
+            var filterVal = 'blur(' + 100 + 'px)';
+            $(".pgBg").css({
+                'filter':filterVal,
+                'webkitFilter':filterVal,
+                'mozFilter':filterVal,
+                'oFilter':filterVal,
+                'msFilter':filterVal,
+                'transition':'all 0.5s ease-out',
+                '-webkit-transition':'all 0.5s ease-out',
+                '-moz-transition':'all 0.5s ease-out',
+                '-o-transition':'all 0.5s ease-out'
+            });
+
+            $('.weatherChooser').fadeIn('fast')
+                .css("display", "absolute")
+                .hide()
+                .fadeIn()
+                .animate({
+                    'top': '0'
+                }, {duration: 'slow', queue: false}, function() {
+                    // Animation complete.
+                });
+        } );
+    }, 500);
+
 });
+ipcRenderer.on('sendSuggestions', (event, suggestions) => {
+    console.log(suggestions)
+    var elementAppend = '';
+
+    suggestions.forEach(function(item){
+        console.log('ID: ' + item.id);
+        elementAppend += '<span class="eachCity">' + item.name + '<br><b>' + item.country + '</b></span>';
+    });
+    $('.suggestionsList').html(elementAppend)
+
+
+});
+window.onkeydown=function(event){
+    if ($('#location').val().trim().length >=3){
+        ipcRenderer.send('getSuggestions', $('#location').val().trim())
+    }
+};
